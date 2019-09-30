e679f955241ae39c076c1470010859909ae4ffd3
khins <kevin@hinshawfam.com>
2019-09-30 16:18:55 -0500

    Implementing Controllers - updates from module 4

--- Globomantics/Startup.cs
+++ Globomantics/Startup.cs
@@ -6,6 +6,7 @@ using Globomantics.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,22 +14,25 @@ namespace Globomantics
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
 
-            //services.AddSingleton<IConferenceService, ConferenceMemoryService>();
-            //easily swap out the memory service with a real API service.
-            //The controller doesn't have to change, and that's exactly the reason why using an interface is recommended.With an interface, 
-            //the controller is decoupled from the actual implementation of the service class. 
-            services.AddSingleton<IConferenceService, ConferenceApiService>();
+            services.AddSingleton<IConferenceService, ConferenceMemoryService>();
+            services.AddSingleton<IProposalService, ProposalMemoryService>();
 
+            services.Configure<GlobomanticsOptions>(configuration.GetSection("Globomantics"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        //This method configures the HTTP request pipeline of ASP.NET Core. The pipeline specifies how the application should respond to HTTP requests.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +40,6 @@ namespace Globomantics
                 app.UseDeveloperExceptionPage();
             }
 
-            //The important one is the StaticFiles middleware.If we don't add that, none of our images, CSS, and JavaScript files will be served to the browser. 
             app.UseStaticFiles();
             app.UseHttpsRedirection();
 
